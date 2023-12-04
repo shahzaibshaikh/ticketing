a12d57d6cc0e561d67e7bf1fb4c87d53df139057
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-12-04 17:40:57 -0500

    📦 NEW: installed express-validator

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,14 @@
 import express from "express";
+import { body } from "express-validator";
 
 const router = express.Router();
 
 router.post("/api/users/signup", (req, res) => {
-  res.send("Hi there.");
+  const { email, password } = req.body;
+
+  if (!email || typeof email !== "string") {
+    res.status(400).send({});
+  }
 });
 
 export { router as signupRouter };
