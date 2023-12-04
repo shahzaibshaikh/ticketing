bfd8fabffcd09f2c1b803c738315c68bee15d9ba
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-12-03 21:31:18 -0500

    🐛 FIX: fixed ingress-srv error

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,9 +1,15 @@
-import express from "express";
-import { json } from "body-parser";
+import express, { Request, Response } from "express";
+import bodyParser from "body-parser";
 
 const app = express();
-app.use(json());
+app.use(bodyParser.json());
 
-app.listen(3000, () => {
-  console.log("Listening on Port 3000.");
+app.get("/api/users/currentuser", (req: Request, res: Response) => {
+  res.send("Hi there.");
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
