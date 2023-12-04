de46066fb336fbd07b44c6628ae03883cc5bc333
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-12-03 21:39:21 -0500

    👌 IMPROVE: added boilerplate for controllers

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,12 +1,11 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import bodyParser from "body-parser";
+import { currentUserRouter } from "./routes/current-user";
 
 const app = express();
 app.use(bodyParser.json());
 
-app.get("/api/users/currentuser", (req: Request, res: Response) => {
-  res.send("Hi there.");
-});
+app.use(currentUserRouter);
 
 const PORT = 3000;
 
