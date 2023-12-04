6a9b9d075b20c83075bddfceb84636d41aa5dad8
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-12-03 21:45:01 -0500

    👌 IMPROVE: added handlers in index.ts

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,11 +1,17 @@
 import express from "express";
 import bodyParser from "body-parser";
 import { currentUserRouter } from "./routes/current-user";
+import { signinRouter } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
+import { signupRouter } from "./routes/signup";
 
 const app = express();
 app.use(bodyParser.json());
 
 app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
 
 const PORT = 3000;
 
