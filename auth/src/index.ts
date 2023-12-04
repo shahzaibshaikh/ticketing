609f109392e91b650c35cfadfa0209ace79a7616
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-12-04 18:17:59 -0500

    📦 NEW: added error-handling middleware

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,7 @@ import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/signout";
 import { signupRouter } from "./routes/signup";
+import { errorHandler } from "./routes/error-handler";
 
 const app = express();
 app.use(bodyParser.json());
@@ -12,6 +13,8 @@ app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
+
+app.use(errorHandler);
 
 const PORT = 3000;
 
