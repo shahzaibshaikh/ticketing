8ff64f3310639e13b98e0459f986d197538e5de0
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-12-04 20:59:08 -0500

    👌 IMPROVE: added custom error base class

--- auth/src/errors/request-validation-error.ts
+++ auth/src/errors/request-validation-error.ts
@@ -1,17 +1,18 @@
 import { ValidationError } from "express-validator";
+import { CustomError } from "./custom-error";
 
-export class RequestValidationError extends Error {
+export class RequestValidationError extends CustomError {
   statusCode = 400;
 
   constructor(public errors: ValidationError[]) {
-    super();
+    super("Invalid request format.");
 
     Object.setPrototypeOf(this, RequestValidationError.prototype);
   }
 
   serializeError() {
     return this.errors.map(err => {
-      return { message: err.msg, field: err.type === "field" && err.path };
+      return { message: err.msg, field: err.type === "field" ? err.path : "" };
     });
   }
 }
--- auth/src/routes/error-handler.ts
+++ auth/src/routes/error-handler.ts
@@ -1,6 +1,5 @@
 import { Request, Response, NextFunction } from "express";
-import { RequestValidationError } from "../errors/request-validation-error";
-import { DatabaseConnectionError } from "../errors/database-connection-error";
+import { CustomError } from "../errors/custom-error";
 
 export const errorHandler = (
   err: Error,
@@ -8,14 +7,7 @@ export const errorHandler = (
   res: Response,
   next: NextFunction
 ) => {
-  if (err instanceof RequestValidationError) {
-    const formattedError = err.errors.map(error => {
-      return { message: error.msg, field: error.type === "field" && error.path };
-    });
-    return res.status(err.statusCode).send({ errors: err.serializeError() });
-  }
-
-  if (err instanceof DatabaseConnectionError) {
+  if (err instanceof CustomError) {
     return res.status(err.statusCode).send({ errors: err.serializeError() });
   }
 
