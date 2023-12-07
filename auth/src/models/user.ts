54e8738733e23cbd1fcd04b02b61d9fea7c881de
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-12-06 21:15:43 -0500

    👌 IMPROVE: added interface for UserDoc

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -5,8 +5,13 @@ interface UserAttributes {
   password: string;
 }
 
-interface UserModel extends mongoose.Model<any> {
-  build(user: UserAttributes): any;
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(user: UserAttributes): UserDoc;
+}
+
+interface UserDoc extends mongoose.Document {
+  email: string;
+  password: string;
 }
 
 const userSchema = new mongoose.Schema({
@@ -24,6 +29,6 @@ userSchema.statics.build = (user: UserAttributes) => {
   return new User(user);
 };
 
-const User = mongoose.model<any, UserModel>("User", userSchema);
+const User = mongoose.model<UserDoc, UserModel>("User", userSchema);
 
 export { User };
