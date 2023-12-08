2dff97b0e5d87ab7a152054f356d1f35f582b4dc
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-12-07 21:02:16 -0500

    📦 NEW: installed cookie-session

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import "express-async-errors";
 import mongoose from "mongoose";
+import cookieSession from "cookie-session";
 
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
@@ -11,7 +12,14 @@ import { errorHandler } from "./routes/error-handler";
 import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
+app.set("trust proxy", true);
 app.use(bodyParser.json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true
+  })
+);
 
 app.use(currentUserRouter);
 app.use(signinRouter);
