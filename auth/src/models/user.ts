883906793e0bc1565bbfadf5b8088c6d9adcb4d2
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-12-08 18:26:53 -0500

    👌 IMPROVE: changed _id to id

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -29,6 +29,8 @@ const userSchema = new mongoose.Schema(
   {
     toJSON: {
       transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
         delete ret.password;
         delete ret.__v;
       }
