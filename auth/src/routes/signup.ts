6e6ff652bd5fb30059f2655afad71673ef5d7a67
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-12-07 21:09:45 -0500

    👌 IMPROVE: signed user and sent token in cookie

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
+import jwt from "jsonwebtoken";
+
 import { RequestValidationError } from "../errors/request-validation-error";
 import { User } from "../models/user";
 import { BadRequestError } from "../errors/bad-request-error";
@@ -31,6 +33,18 @@ router.post(
 
     const user = User.build({ email, password });
     await user.save();
+
+    const token = jwt.sign(
+      {
+        id: user.id,
+        email: user.email
+      },
+      "asdasdkjanxskjasnxkjansxkjniuqwhdwq"
+    );
+
+    req.session = {
+      jwt: token
+    };
 
     res.status(201).send(user);
   }
