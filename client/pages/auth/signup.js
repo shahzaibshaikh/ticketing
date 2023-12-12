c1df2792385d17ed2c1390ce408f00ad7f16ea67
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-12-12 18:13:30 -0500

    👌 IMPROVE: added two way state binding

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,16 +1,28 @@
-import React from "react";
+import { useState } from "react";
 
 const signup = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <form>
       <h1>Sign Up</h1>
       <div className='form-group'>
         <label>Email Address</label>
-        <input className='form-control' />
+        <input
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+          className='form-control'
+        />
       </div>
       <div className='form-group'>
         <label>Password</label>
-        <input className='form-control' type='password' />
+        <input
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+          className='form-control'
+          type='password'
+        />
       </div>
       <button className='btn btn-primary'>Sign Up</button>
     </form>
