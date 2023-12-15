d2f0b74ee7be47a9b65c2962b775244b504aa063
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-12-14 21:26:41 -0500

    📦 NEW: added LandingPage component

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,6 +1,14 @@
 import React from "react";
+import axios from "axios";
 
-const index = () => {
+const LandingPage = ({ currentUser }) => {
   return <div>Index Route</div>;
 };
-export default index;
+
+LandingPage.getInitialProps = async () => {
+  const response = await axios.get("/api/users/currentuser");
+
+  return response.data;
+};
+
+export default LandingPage;
