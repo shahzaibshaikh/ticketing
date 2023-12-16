e3c3316da03efb3977ecebd5ed04db0f6a2f95f3
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-12-16 18:32:06 -0500

    👌 IMPROVE: identified browser/server

--- client/pages/index.js
+++ client/pages/index.js
@@ -7,7 +7,10 @@ const LandingPage = ({ currentUser }) => {
 };
 
 LandingPage.getInitialProps = async () => {
-  const response = await axios.get("/api/users/currentuser");
+  if (typeof window === "undefined") {
+  } else {
+    const response = await axios.get("/api/users/currentuser");
+  }
 
   return response.data;
 };
