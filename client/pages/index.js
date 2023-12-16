bc5ab659f50031c771815f8efa109ae06fd77e94
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-12-16 18:35:46 -0500

    👌 IMPROVE: added complete route path

--- client/pages/index.js
+++ client/pages/index.js
@@ -9,7 +9,7 @@ const LandingPage = ({ currentUser }) => {
 LandingPage.getInitialProps = async () => {
   if (typeof window === "undefined") {
     const { data } = await axios.get(
-      "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local"
+      "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser"
     );
     return data;
   } else {
