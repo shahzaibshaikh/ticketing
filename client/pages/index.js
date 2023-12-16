1329124db796349ffbe09619c80d2893343be9cc
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-12-16 18:37:59 -0500

    👌 IMPROVE: added host to request

--- client/pages/index.js
+++ client/pages/index.js
@@ -9,7 +9,12 @@ const LandingPage = ({ currentUser }) => {
 LandingPage.getInitialProps = async () => {
   if (typeof window === "undefined") {
     const { data } = await axios.get(
-      "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser"
+      "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
+      {
+        headers: {
+          Host: "ticketing.dev"
+        }
+      }
     );
     return data;
   } else {
