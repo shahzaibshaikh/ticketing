e20e3f0b2357386c2394d20e38596ee02587976d
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-12-19 21:24:56 -0500

    👌 IMPROVE: utilized buildClient

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,26 +1,14 @@
 import React from "react";
-import axios from "axios";
+import buildClient from "../api/buildClient";
 
 const LandingPage = ({ currentUser }) => {
   console.log(currentUser);
   return <div>Index Route</div>;
 };
 
-LandingPage.getInitialProps = async ({ req }) => {
-  if (typeof window === "undefined") {
-    const { data } = await axios.get(
-      "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
-      {
-        headers: req.headers
-      }
-    );
-    return data;
-  } else {
-    const { data } = await axios.get("/api/users/currentuser");
-    return data;
-  }
-
-  return {};
+LandingPage.getInitialProps = async context => {
+  const { data } = await buildClient(context).get("/api/users/currentuser");
+  return data;
 };
 
 export default LandingPage;
