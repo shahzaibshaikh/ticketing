04b7a523633be96120ac61f0cc815cef8b52d114
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-12-19 21:31:30 -0500

    👌 IMPROVE: created new file for signin

---
+++ client/pages/auth/signin.js
@@ -0,0 +1,52 @@
+import axios from "axios";
+import { useState } from "react";
+import useRequest from "../../hooks/useRequest";
+import { useRouter } from "next/router";
+
+const signin = () => {
+  const router = useRouter();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { doRequest, errors } = useRequest({
+    url: "/api/users/signin",
+    method: "post",
+    body: {
+      email,
+      password
+    },
+    onSuccess: () => router.push("/")
+  });
+
+  const onSubmit = async event => {
+    event.preventDefault();
+
+    doRequest();
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <h1>Sign Up</h1>
+      <div className='form-group'>
+        <label>Email Address</label>
+        <input
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+          className='form-control'
+        />
+      </div>
+      <div className='form-group'>
+        <label>Password</label>
+        <input
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+          className='form-control'
+          type='password'
+        />
+      </div>
+      {errors}
+      <button className='btn btn-primary'>Sign Up</button>
+    </form>
+  );
+};
+
+export default signup;
--- client/pages/banana.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-const banana = () => {
-  return <h1>Banana2</h1>;
-};
-export default banana;
