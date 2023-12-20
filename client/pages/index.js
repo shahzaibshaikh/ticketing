fa7ff86747cfc407d511438704bf4d0e7a7238f0
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-12-19 21:16:41 -0500

    👌 IMPROVE: passed cookies to nginx

--- client/pages/index.js
+++ client/pages/index.js
@@ -6,14 +6,12 @@ const LandingPage = ({ currentUser }) => {
   return <div>Index Route</div>;
 };
 
-LandingPage.getInitialProps = async () => {
+LandingPage.getInitialProps = async ({ req }) => {
   if (typeof window === "undefined") {
     const { data } = await axios.get(
       "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
       {
-        headers: {
-          Host: "ticketing.dev"
-        }
+        headers: req.headers
       }
     );
     return data;
