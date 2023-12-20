e2e38185c7d18a619e1c8d5ad722441fbcfde905
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-12-19 21:29:48 -0500

    👌 IMPROVE: rendered sign in check

--- client/pages/index.js
+++ client/pages/index.js
@@ -2,8 +2,7 @@ import React from "react";
 import buildClient from "../api/buildClient";
 
 const LandingPage = ({ currentUser }) => {
-  console.log(currentUser);
-  return <div>Index Route</div>;
+  return currentUser ? <h1>You are signed in</h1> : <h1>You are not signed in</h1>;
 };
 
 LandingPage.getInitialProps = async context => {
