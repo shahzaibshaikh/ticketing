64025cb1e868ad679128b5abb51b5b3902410e35
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-12-20 21:25:39 -0500

    👌 IMPROVE: rendered signin/out

--- client/components/Header.js
+++ client/components/Header.js
@@ -7,6 +7,11 @@ const Header = ({ currentUser }) => {
       <Link href='/'>
         <a className='navbar-brand'>GitTix</a>
       </Link>
+      <div className='d-flex justify-content-end'>
+        <ul className='nav d-flex align-items-center'>
+          {currentUser ? "Sign Out" : "Sign In"}
+        </ul>
+      </div>
     </nav>
   );
 };
