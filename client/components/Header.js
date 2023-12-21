3ed26322670d6719637eec8c54901a82dd3c7b78
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-12-20 21:37:36 -0500

    👌 IMPROVE: dynamical rendering of buttons

--- client/components/Header.js
+++ client/components/Header.js
@@ -2,16 +2,23 @@ import React from "react";
 import Link from "next/link";
 
 const Header = ({ currentUser }) => {
-  console.log(currentUser);
+  const links = [
+    !currentUser && { label: "Sign Up", href: "/auth/signup" },
+    !currentUser && { label: "Sign In", href: "/auth/signin" },
+    currentUser && { label: "Sign Out", href: "/auth/signout" }
+  ]
+    .filter(linkConfig => linkConfig)
+    .map(({ label, href }) => {
+      return <li key={href}>{label}</li>;
+    });
+
   return (
     <nav className='navbar navbar-light bg-light'>
       <Link href='/' className='navbar-brand'>
         GitTix
       </Link>
       <div className='d-flex justify-content-end'>
-        <ul className='nav d-flex align-items-center'>
-          {currentUser ? "Sign Out" : "Sign In"}
-        </ul>
+        <ul className='nav d-flex align-items-center'>{links}</ul>
       </div>
     </nav>
   );
